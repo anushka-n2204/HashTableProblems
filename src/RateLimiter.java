6d00ed1f00b7b24ca4e26fef09dec6abcd279adb
anuushk-n2204 <anushkanayak530@gmail.com>
2026-03-10 20:45:52 +0530

    Problem6: Distributed API rate limiter

--- src/RateLimiter.java
+++ src/RateLimiter.java
@@ -1,2 +1,43 @@
+import java.util.*;
+
 public class RateLimiter {
+
+    static class TokenBucket{
+
+        int tokens;
+
+        TokenBucket(){
+            tokens = 5;
+        }
+    }
+
+    static HashMap<String,TokenBucket> clients = new HashMap<>();
+
+    public static boolean checkRateLimit(String clientId){
+
+        clients.putIfAbsent(clientId,new TokenBucket());
+
+        TokenBucket bucket = clients.get(clientId);
+
+        if(bucket.tokens>0){
+
+            bucket.tokens--;
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        String client="abc123";
+
+        for(int i=1;i<=7;i++){
+
+            if(checkRateLimit(client))
+                System.out.println("Request "+i+" allowed");
+            else
+                System.out.println("Rate limit exceeded");
+        }
+    }
 }
