b0b8eb9e684d76e7e0418e447f9580cf6c0cde39
anuushk-n2204 <anushkanayak530@gmail.com>
2026-03-10 20:28:48 +0530

    Base HashTableProblems application setup

---
+++ src/HashTableProblemsApp.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class HashTableProblemsApp {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("Hash Table Problems Application");
+
+    }
+
+}
