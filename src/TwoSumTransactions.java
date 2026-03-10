278c8c618702f7543a088fb7d5f9f12af04ce3d4
anuushk-n2204 <anushkanayak530@gmail.com>
2026-03-10 20:46:26 +0530

    Problem9: Two-sum financial transactions

--- src/TwoSumTransactions.java
+++ src/TwoSumTransactions.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class TwoSumTransactions {
+
+    public static int[] twoSum(int[] nums,int target){
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+
+            int complement = target-nums[i];
+
+            if(map.containsKey(complement))
+                return new int[]{map.get(complement),i};
+
+            map.put(nums[i],i);
+        }
+
+        return new int[]{-1,-1};
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {500,300,200};
+
+        int[] result = twoSum(nums,500);
+
+        System.out.println("Indexes: "+result[0]+" "+result[1]);
+    }
 }
